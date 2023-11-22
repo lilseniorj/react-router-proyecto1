07813928c69bf1f2926a1014860f440b3e6c65ed
lilseniorj <jesusvarguer18@gmail.com>
2023-11-22 16:53:25 -0500

    Roles y Permisos

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,14 +1,19 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { blogdata } from "./blogdata";
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAuth } from './auth';
+import { blogdata } from './blogdata'
 
 function BlogPost() {
   const navigate = useNavigate();
   const { slug } = useParams();
 
+  const auth = useAuth();
+
   const blogpost = blogdata.find(post => post.slug === slug);
 
-  const returnToBlog = () =>{
+  const canDelete = auth.user?.isAdmin || blogpost.author === auth.user?.username;
+
+  const returnToBlog = () => {
     navigate('/blog');
   };
 
@@ -16,10 +21,14 @@ function BlogPost() {
     <>
       <h2>{blogpost.title}</h2>
       <button onClick={returnToBlog}>Volver al blog</button>
-      <p>{blogpost.content}</p>
       <p>{blogpost.author}</p>
+      <p>{blogpost.content}</p>
+
+      {canDelete && (
+        <button>Eliminar blogpost</button>
+      )}
     </>
-  )
+  );
 }
 
 export { BlogPost };
